4ff9ae59b649ece740aaa1467a31f880dc5aa932
QuantumRange <justusst@mailbox.org>
2022-02-06 20:55:13 +0100

    Adding Project

---
+++ src/main/java/de/quantumrange/openuntis/OpenuntisApplication.java
@@ -0,0 +1,13 @@
+package de.quantumrange.openuntis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenuntisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenuntisApplication.class, args);
+	}
+
+}
